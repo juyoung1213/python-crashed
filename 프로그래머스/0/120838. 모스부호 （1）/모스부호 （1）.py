7f76386506c244326b2f868984f449b762ba50a2
juyoung1213 <1437rhwndud@naver.com>
2024-05-31 11:10:29 +0900

    [level 0] Title: 모스부호 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -1,5 +1,4 @@
 def solution(letter):
-    # 모스 부호와 알파벳을 매핑하는 딕셔너리
     morse_code = {
         '.-': 'a', '-...': 'b', '-.-.': 'c', '-..': 'd', '.': 'e',
         '..-.': 'f', '--.': 'g', '....': 'h', '..': 'i', '.---': 'j',
@@ -9,15 +8,8 @@ def solution(letter):
         '--..': 'z'
     }
     
-    # 주어진 문자열을 공백으로 나눠서 모스 부호 리스트로 변환
     morse_letters = letter.split(' ')
     
-    # 모스 부호를 알파벳으로 변환하여 리스트에 저장
     decoded_letters = [morse_code[morse] for morse in morse_letters]
     
-    # 리스트를 문자열로 변환하여 반환
     return ''.join(decoded_letters)
-
-# 예제 사용
-print(solution(".... . .-.. .-.. ---"))  # "hello"
-print(solution(".--. -.-- - .... --- -."))  # "python"
