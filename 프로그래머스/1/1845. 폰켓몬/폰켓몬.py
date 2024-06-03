0bd5bc229fa620d222bc152f5bb5dd88ea17a1f0
juyoung1213 <1437rhwndud@naver.com>
2024-06-03 09:18:04 +0900

    [level 1] Title: 폰켓몬, Time: 0.75 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,16 @@
-def solution(nums):
+"""def solution(nums):
     unique_pokemon = set(nums)
     
     max_pokemon = len(nums) // 2
     
-    return min(len(unique_pokemon), max_pokemon)
+    return min(len(unique_pokemon), max_pokemon)"""
 
+def solution(nums):
+    my_dict = {}
+    for num in nums:
+        if num not in my_dict:
+            my_dict[num] = True
+    if len(my_dict) >= len(nums) // 2:
+        return len(nums) // 2
+    else:
+        return len(my_dict)
