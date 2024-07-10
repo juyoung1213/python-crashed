ea45c7eeff7f9e5ab03910e990506bb7e8ea9bb7
juyoung1213 <1437rhwndud@naver.com>
2024-07-10 20:39:42 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,16 +1,15 @@
 def solution(food):
-    # 'left_side' 문자열을 빈 문자열로 초기화
-    left_side=""
+    left_side="" # 왼쪽 부분을 저장할 변수 초기화
     
-    # 1번 음식부터 차례로 배치를 계산함.
+    #각 음식 종류에 대해 반복함(1번 음식부터 시작)
     for i in range(1, len(food)):
-        # 해당 음식의 사용 가능한 양을 계산(2로 나눈 몫)
-        count = food[i] // 2
-        # 왼쪽 부분에 음식을 추가함.
-        left_side += str(i) * count
+        count = food[i] // 2 # 사용할 수 있는 음식 양(2로 나눈 몫)
         
-    # 왼쪽 부분과 그 반대 순서의 오른쪽 부분을 결합하고, 중앙에 물을 추가함.
+        # 해당 음식의 양이 1 이상이면
+        if count > 0:
+            left_side += str(i) * count # 왼쪽 부분에 추가
+            
     right_side = left_side[::-1] # 오른쪽 부분은 왼쪽 부분의 반대 순서
-    result = left_side + "0" + right_side
+    result = left_side + "0" + right_side # 최종 결과 문자열
     
     return result
